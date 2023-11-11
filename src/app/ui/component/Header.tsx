8392c0fd8f41448e73e16130c7a14b600a735d2e
t3di <tediaga.c@gmail.com>
2023-11-11 01:05:08 +0100

    fix: remove not existing component

--- src/app/ui/component/Header.tsx
+++ src/app/ui/component/Header.tsx
@@ -1,19 +1,16 @@
 import React from "react";
-import Hero from "./Hero";
 
 const Header = () => {
   return (
     <header>
-      <Hero>
-        <h1> Thouaré Rugby </h1>
-        <p>
-          Découvrez le monde passionnant du rugby comme jamais auparavant et
-          rejoignez notre mission pour créer un club de rugby dynamique au cœur
-          de notre ville. Plongez dans l&#39;action, l&#39;histoire et la
-          culture de ce sport emblématique, et ensemble, faisons du rêve
-          d&#39;un club de rugby local une réalité !
-        </p>
-      </Hero>
+      <h1> Thouaré Rugby </h1>
+      <p>
+        Découvrez le monde passionnant du rugby comme jamais auparavant et
+        rejoignez notre mission pour créer un club de rugby dynamique au cœur de
+        notre ville. Plongez dans l&#39;action, l&#39;histoire et la culture de
+        ce sport emblématique, et ensemble, faisons du rêve d&#39;un club de
+        rugby local une réalité !
+      </p>
     </header>
   );
 };
